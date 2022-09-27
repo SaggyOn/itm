adbf8fca5ddc2c258bb91b55f9d525f822c7cb94
SaggyOn <sagarrasal333@gmail.com>
2022-09-27 08:16:49 +0530

    massage

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,324 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <section class="maincolor">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <p class="text-white">
+              Business education is at the very core of ITM Group of
+              Institutions, and ITM Business School is our flagship institution.
+              Since 1991, thousands of graduates have moved into successful
+              careers and our Alumni network is spread across the globe.
+            </p>
+          </div>
+          <div class="col-md-6">
+            <p style="margin-bottom: 2px" class="text-white">
+              <strong>Campuses at :</strong> <br />
+              <button type="button" class="btn btn-outline-light">
+                Navi Mumbai
+              </button>
+              <button type="button" class="btn btn-outline-light">
+                Vadodara
+              </button>
+              <button type="button" class="btn btn-outline-light">
+                Raipur
+              </button>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="works">
+      <div class="container">
+        <div class="custom-card">
+          <div class="row pt-3 pt-xl-5 row-gap">
+            <div class="col-6 col-md-6 col-lg-3">
+              <div class="media">
+                <figure class="icon icon-light-green">
+                  <img
+                    src="https://dg1xqmhtoint1.cloudfront.net/img/common-icon/certificate.webp?"
+                    class="img-fluid img-animation"
+                    alt="video"
+                  />
+                </figure>
+                <div>
+                  <span class="text-grey font-small-1">
+                    AICTE, AIU and NBA-Approved
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div class="col-6 col-md-6 col-lg-3">
+              <div class="media">
+                <figure class="icon icon-light-blue">
+                  <img
+                    src="https://dg1xqmhtoint1.cloudfront.net/img/common-icon/tiesup.webp?"
+                    class="img-fluid img-animation"
+                    alt="video"
+                  />
+                </figure>
+                <div>
+                  <span class="text-grey font-small-1">
+                    650+ Placements Partners
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div class="col-6 col-md-6 col-lg-3">
+              <div class="media">
+                <figure class="icon icon-dark-blue">
+                  <img
+                    src="https://dg1xqmhtoint1.cloudfront.net/img/common-icon/certificate.webp?"
+                    class="img-fluid img-animation"
+                    alt="video"
+                  />
+                </figure>
+                <div>
+                  <span class="text-grey font-small-1">
+                    5 Campuses &amp; Seasoned Faculties
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div class="col-6 col-md-6 col-lg-3">
+              <div class="media">
+                <figure class="icon icon-red">
+                  <img
+                    src="https://dg1xqmhtoint1.cloudfront.net/img/common-icon/tiesup.webp?"
+                    class="img-fluid img-animation"
+                    alt="video"
+                  />
+                </figure>
+                <div>
+                  <span class="text-grey font-small-1"> 45000+ Alumni </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container h-100">
+        <div class="row align-middle">
+          <div class="col-md-6 col-lg-4 column">
+            <div class="card gr-1">
+              <div class="txt">
+                <h1>BRANDING AND CORPORATE DESIGN</h1>
+                <p>Visual communication and problem-solving</p>
+              </div>
+              <a href="#">more</a>
+              <div class="ico-card">
+                <i class="fa fa-rebel"></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 column">
+            <div class="card gr-2">
+              <div class="txt">
+                <h1>Web Front-End SOLUTIONS</h1>
+                <p>How design is implemented on the web.</p>
+              </div>
+              <a href="#">more</a>
+              <div class="ico-card">
+                <i class="fa fa-codepen"></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 column">
+            <div class="card gr-3">
+              <div class="txt">
+                <h1>UX/UI WEBsite AND MOBILE app</h1>
+                <p>User Interface and User Experience Design.</p>
+              </div>
+              <a href="#">more</a>
+              <div class="ico-card">
+                <i class="fa fa-empire"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section
+      class="
+        clients_logo
+        section-padding
+        dstudentsarefrom
+        bg-gray
+        dignifiedshow
+      "
+      id="placements"
+      style="clear: both"
+    >
+      <div class="container">
+        <div class="row">
+          <div class="text-center col-sm-12">
+            <h3>Get your Dream Job!</h3>
+            <p>
+              Our amazing placement team has cracked 100% placement every year
+              since 2015 and managed this feat even through the pandemic. With
+              over 650 recruiters coming on campus every year, you will have
+              your choice of brand names and job profiles. From unicorn startups
+              to ET100 top companies, we bring the best jobs, top salaries, and
+              international opportunities right to you.
+            </p>
+          </div>
+          <div class="col-lg-3 col-md-6">
+            <div class="live-border">
+              <div class="border top">
+                <div class="bar"></div>
+              </div>
+              <div class="border right">
+                <div class="bar"></div>
+              </div>
+              <div class="border bottom">
+                <div class="bar"></div>
+              </div>
+              <div class="border left">
+                <div class="bar"></div>
+              </div>
+              <div class="item text-center mb-md50">
+                <h3><span class="">21</span></h3>
+                <h6>LPA/Highest Salary Package</h6>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6">
+            <div class="live-border">
+              <div class="border top">
+                <div class="bar"></div>
+              </div>
+              <div class="border right">
+                <div class="bar"></div>
+              </div>
+              <div class="border bottom">
+                <div class="bar"></div>
+              </div>
+              <div class="border left">
+                <div class="bar"></div>
+              </div>
+              <div class="item text-center mb-md50">
+                <h3><span class="">8.65</span></h3>
+                <h6>LPA/Average Salary Package</h6>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6">
+            <div class="live-border">
+              <div class="border top">
+                <div class="bar"></div>
+              </div>
+              <div class="border right">
+                <div class="bar"></div>
+              </div>
+              <div class="border bottom">
+                <div class="bar"></div>
+              </div>
+              <div class="border left">
+                <div class="bar"></div>
+              </div>
+              <div class="item text-center mb-md50">
+                <h3><span class="">100%</span></h3>
+                <h6>Students placed</h6>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6">
+            <div class="live-border">
+              <div class="border top">
+                <div class="bar"></div>
+              </div>
+              <div class="border right">
+                <div class="bar"></div>
+              </div>
+              <div class="border bottom">
+                <div class="bar"></div>
+              </div>
+              <div class="border left">
+                <div class="bar"></div>
+              </div>
+              <div class="item text-center mb-md50">
+                <h3><span class="">650+</span></h3>
+                <h6>Company Visits for placements</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="specializationwiseplacement">
+          <div class="container">
+            <h3>Placements Stats</h3>
+            <table>
+              <tr>
+                <td>Specialization</td>
+                <td>Highest Salary</td>
+                <td>Average Salary</td>
+              </tr>
+              <tr>
+                <td>Marketing</td>
+                <td>₹21L</td>
+                <td>₹8.5L</td>
+              </tr>
+              <tr>
+                <td>Finance</td>
+                <td>₹14L</td>
+                <td>₹8L</td>
+              </tr>
+              <tr>
+                <td>Digital Marketing &amp; Transformation</td>
+                <td>₹8.5L</td>
+                <td>₹7.2L</td>
+              </tr>
+              <tr>
+                <td>Supply Chain &amp; Operation Management</td>
+                <td>₹12.4L</td>
+                <td>₹6.5L</td>
+              </tr>
+              <tr>
+                <td>Human Resource &amp; Management</td>
+                <td>₹8.5L</td>
+                <td>₹6.5L</td>
+              </tr>
+              <tr>
+                <td>International Business</td>
+                <td>₹15L</td>
+                <td>₹7.75L</td>
+              </tr>
+              <tr>
+                <td>Retail Management &amp; Marketing</td>
+                <td>₹10.4L</td>
+                <td>₹7L</td>
+              </tr>
+              <tr>
+                <td>Financial Markets</td>
+                <td>₹10.8L</td>
+                <td>₹6.5L</td>
+              </tr>
+              <tr>
+                <td>Business Analytics</td>
+                <td>₹12.5L</td>
+                <td>₹7.5L</td>
+              </tr>
+              <tr>
+                <td>Fintech</td>
+                <td>₹9.2L</td>
+                <td>₹7L</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+export default {};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+@import "../assets/css/styles.scss";
+@import "../assets/css/style.css";
+</style>
+<style lang="scss">
 </style>
